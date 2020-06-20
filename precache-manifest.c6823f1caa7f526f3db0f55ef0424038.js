7e59cb454ddc891f05e743257ac5627cd417c6c0
Akshay <upadhyayakshay91197@gmail.com>
2020-06-20 21:52:26 +0530

    Updates

---
+++ precache-manifest.c6823f1caa7f526f3db0f55ef0424038.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9391e5feecbd28f0420ec879099c1e3d",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "c7bb2405f43aad8cbe57",
+    "url": "/contactmanager/static/css/2.d5fa9f53.chunk.css"
+  },
+  {
+    "revision": "e3a3dc84f54bef5c0643",
+    "url": "/contactmanager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c7bb2405f43aad8cbe57",
+    "url": "/contactmanager/static/js/2.9c415c56.chunk.js"
+  },
+  {
+    "revision": "2ecd760a01b9b08172f3fbed44ec5739",
+    "url": "/contactmanager/static/js/2.9c415c56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3a3dc84f54bef5c0643",
+    "url": "/contactmanager/static/js/main.b7c2c15d.chunk.js"
+  },
+  {
+    "revision": "71d3a297f4256315968e",
+    "url": "/contactmanager/static/js/runtime-main.d7d94ad5.js"
+  }
+]);
